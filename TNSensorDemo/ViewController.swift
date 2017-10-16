5a20a8d8e85c38c549c7ce9c084feeeee147af50
merrill <243203681@qq.com>
2017-10-16 17:24:29 +0800

    Initial Commit

---
+++ TNSensorDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TNSensorDemo
+//
+//  Created by merrill on 2017/10/16.
+//  Copyright © 2017年 merrill. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
